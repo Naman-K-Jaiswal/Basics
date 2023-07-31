83f93e69f04ed5e96bb4142332169ca8486029b2
Jaiswal-on-Fire <namankkumarjjaiswal@gmail.com>
2023-07-31 23:37:31 +0530

    Jump Added

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
+    public LayerMask PlatformLayerMask;
+    public Collider2D gd;
     public Animator anim;
+    public float jumpForce = 10.0f;
     public float speed = 10.0f;
+    bool isJumping = false;
+    // private bool isGrounded = true;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        //gd = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float dir_x = Input.GetAxisRaw("Horizontal");
+        //float disttoGround = gd.bounds.extents.y;
 
        if(dir_x != 0)
        {
@@ -28,5 +35,35 @@ public class movement : MonoBehaviour
 
         Vector3 direction = new Vector3(dir_x * speed * Time.deltaTime, 0, 0);
         transform.position += direction;
+
+        // if(Physics.Raycast(transform.position, -Vector3.up, disttoGround + 0.1f, PlatformLayerMask)){
+        //     Debug.Log("AIr");
+        //     isJumping = false;
+        //      isGrounded = true;
+        // }
+
+
+        if (IsGrounded() && Input.GetButtonDown("Jump")){
+            Vector3 JumpDirection = Vector3.up;
+            rb.AddForce(JumpDirection*jumpForce);
+            // isGrounded = false;
+            isJumping = true;
+        }
+
+
+        anim.SetBool("jump", !IsGrounded());
+    }
+
+    //void OnTriggerEnter(Collider other){
+        
+    //     if (other.CompareTag("Platform")){
+    //         // isGrounded = true;
+    //         Debug.Log("Yo");
+    //         isJumping = false;
+    //     }
+    private bool IsGrounded(){
+        float disttoGround = gd.bounds.extents.y;
+        RaycastHit2D raycastHit = Physics2D.Raycast(gd.bounds.center, -Vector3.up, disttoGround + 0.5f, PlatformLayerMask);
+        return raycastHit.collider != null;
     }
 }
